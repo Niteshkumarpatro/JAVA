b08261a6d02bbaf1d234af9171c5153b48d1aec7
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-07 16:12:56 +0530

    first commit

---
+++ basic.java
@@ -0,0 +1,7 @@
+public class basic{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("my name is NITESH KUMAR PATRO");
+         
+    }
+}
