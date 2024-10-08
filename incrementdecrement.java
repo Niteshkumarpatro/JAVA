e3cd77cfa3cc922972e39da5842adadd27680b34
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-08 22:49:45 +0530

    increment/decrement

---
+++ incrementdecrement.java
@@ -0,0 +1,27 @@
+public class incrementdecrement {
+    public static void main(String[] args) {
+         int x=5;
+        System.out.println(x);
+         x++;
+        System.out.println(x);
+         x--;
+        System.out.println(x); 
+        
+        System.out.println(++x);
+        
+         System.out.println(--x);
+         // x++ is "use x and then increment" this is post incrrement operator
+         //  ++x is "increment x and then use " this is pre increment operator
+
+
+
+
+        
+
+
+
+
+        
+    }
+    
+}
