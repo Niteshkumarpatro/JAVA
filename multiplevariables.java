007f014b1a36ce199ef53d307c08b4001ac8029f
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-08 22:56:09 +0530

    multiplesvariable

---
+++ multiplevariables.java
@@ -0,0 +1,22 @@
+public class multiplevariables {
+    public static void main(String[] args) {
+
+     int x=1,y=2,z=3;
+
+     System.out.println("the value of x is "+x);
+     
+     System.out.println("the value of y is "+y);
+     
+     z = 10;
+     z = x+1;
+
+     System.out.println("the value of z is "+z);
+     System.out.println("kamal dekho " + x + y + z );
+     System.out.println("" + x + y + z );
+     System.out.println(x+""+y+z);
+     
+     
+     
+     
+    }
+}
