e2f9ba4d1787e9969a86e49e67bfd37be5315aa2
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-08 11:13:40 +0530

    variables

---
+++ variablesint.java
@@ -0,0 +1,21 @@
+public class variablesint {
+    public static void main(String[] args) {
+         int x = 500;
+         x = 500;
+         System.out.println(x);
+
+         x = 400;
+         System.out.println(x);
+         System.out.println(x+"1");
+         System.out.println(x+1);
+
+         x = x + 5;
+         System.out.println(x);
+          
+         x = x - 10;
+         System.out.println(x);
+         
+        
+    }
+    
+}
