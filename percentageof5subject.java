94ae70c118e0fe115c3f7927078a66364008cf22
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-09 13:34:09 +0530

    #2PROGRAM

---
+++ percentageof5subject.java
@@ -0,0 +1,13 @@
+public class percentageof5subject{
+    public static void main(String[] args) {
+        double a=91; // maths
+        double b=93; // english
+        double c=99; // physiscs
+        double d=90; // chemistry
+        double e=89; // biology
+
+        double percentage=a+b+c+d+e/5;
+
+         System.out.println("the percentage of 5 marks is " + percentage);
+    }
+}
