77c2e3aa41688abd2453a11fb38cc5ebbc0b7848
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-09 16:58:46 +0530

    #4PROGRAM

---
+++ volumeofsphere.java
@@ -0,0 +1,9 @@
+public class volumeofsphere {
+    public static void main(String[] args) {
+        double radius = 12;
+         double pi= 3.14;
+        double volume=(4.0/3.0)*pi*(radius*radius*radius);
+        System.out.println("the volume of sphere is "+volume);
+    
+    }
+}
