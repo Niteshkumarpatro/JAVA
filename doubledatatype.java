cd17ac601d9fcd5381ddc99184808b9768ffbb28
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-09 13:33:44 +0530

    doubledatatype

---
+++ doubledatatype.java
@@ -0,0 +1,16 @@
+public class doubledatatype {
+        public static void main(String[] args) {
+        double x = 5;
+        double y = 2;
+        double z = x  / y;
+        
+        System.out.println(z);
+
+       /* 
+       double a = 5 / 2; it treat as a integer bcz these two numbers are integer
+         double b = 5.0/2; it treat as double bcz here bca one of them is float 5/2=2
+        5.0/2=2.5
+        5/2.0=2.5
+        5.0/2.0=2.5 */
+    }
+}
