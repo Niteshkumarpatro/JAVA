e3fe4857c666b31c8e285e44a1f76f95f3392532
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-09 16:57:57 +0530

    #4PROGRAM

---
+++ circumferance.java
@@ -0,0 +1,8 @@
+public class circumferance{
+    public static void main(String[] args) {
+        double radius=4;
+        double pi=3.14;
+        double circumferance=2*pi*radius;
+        System.out.println(circumferance);
+    }
+}
