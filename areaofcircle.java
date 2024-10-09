be67b0d9530b6f219b1b0cba2499fa3eef152bba
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-09 16:57:17 +0530

    #3PROGRAM

---
+++ areaofcircle.java
@@ -0,0 +1,9 @@
+public class areaofcircle {
+    public static void main(String[] args) {
+         double radius=19;
+         double pi=3.14;
+         double area=pi*radius*radius;
+          System.out.println("the area of circle is " +area);
+    }
+    
+}
