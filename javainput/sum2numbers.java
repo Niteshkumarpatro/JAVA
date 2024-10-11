f224c1021ad065da5db1009db20b185517881bee
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-11 13:31:57 +0530

    #8PROGRAM

---
+++ javainput/sum2numbers.java
@@ -0,0 +1,17 @@
+package javainput;
+import java.util.Scanner;
+
+public class sum2numbers {
+public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print(" Enter first number ");
+    int first= sc.nextInt();
+    System.out.print(" Enter second number ");
+    int second= sc.nextInt();
+     int sum=first+second;
+     System.out.println(" The sum of two numbers is " +sum);
+
+     
+         
+    }
+}
