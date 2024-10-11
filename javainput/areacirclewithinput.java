3627eecdee47a73cf9a16f80f46213937025ae62
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-11 13:31:09 +0530

    #6PROGRAM

---
+++ javainput/areacirclewithinput.java
@@ -0,0 +1,21 @@
+package javainput;
+ import java.util.Scanner;
+
+public class areacirclewithinput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter radius ");
+        int radius = sc.nextInt();
+        double pi =3.14;
+        double area = pi*radius*radius;
+        System.out.println("The area of the circle is " +area);
+
+        
+
+
+
+
+        
+    }
+    
+}
