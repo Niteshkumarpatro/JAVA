dc00d67727ef4066450d067d6e3a270a3c9bc5bd
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-11 13:32:17 +0530

    #9PROGRAM

---
+++ javainput/squareofnumber.java
@@ -0,0 +1,13 @@
+package javainput;
+import java.util.Scanner;
+public class squareofnumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    System.out.print(" Enter number ");
+    int number =sc.nextInt();
+    int square=(number*number);
+    System.out.println("The square of the number is " +square);
+
+    }
+    
+}
