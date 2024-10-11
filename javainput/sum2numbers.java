ef1e773e34e457d00ac56b1c14e7a65bd53474cc
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-11 13:55:53 +0530

    #8PROGRAM

--- javainput/sum2numbers.java
+++ javainput/sum2numbers.java
@@ -3,13 +3,14 @@ import java.util.Scanner;
 
 public class sum2numbers {
 public static void main(String[] args){
-    Scanner sc = new Scanner(System.in);
-    System.out.print(" Enter first number ");
-    int first= sc.nextInt();
-    System.out.print(" Enter second number ");
-    int second= sc.nextInt();
-     int sum=first+second;
-     System.out.println(" The sum of two numbers is " +sum);
+    try (Scanner sc = new Scanner(System.in)) {
+        System.out.print(" Enter first number ");
+        int first= sc.nextInt();
+        System.out.print(" Enter second number ");
+        int second= sc.nextInt();
+         int sum=first+second;
+         System.out.println(" The sum of two numbers is " +sum);
+    }
 
      
          
