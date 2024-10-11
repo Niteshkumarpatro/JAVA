369d7268a5640538a9a91c98bb2cbd9bf03986cf
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-11 13:55:36 +0530

    #7PROGRAM

--- javainput/findremainder.java
+++ javainput/findremainder.java
@@ -2,14 +2,15 @@ package javainput;
 import java.util.Scanner;
 public class findremainder {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.print(" Enter dividend ");
-        int dividend = sc.nextInt();
-        System.out.print(" Enter divisor ");
-        int divisor =sc.nextInt();
-        int quotient=(dividend/divisor);
-        int remainder=dividend-(divisor*quotient);
-        System.out.println("The remainder when "+dividend+"  is divided by  "+divisor+" is " +remainder);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print(" Enter dividend ");
+            int dividend = sc.nextInt();
+            System.out.print(" Enter divisor ");
+            int divisor =sc.nextInt();
+            int quotient=(dividend/divisor);
+            int remainder=dividend-(divisor*quotient);
+            System.out.println("The remainder when "+dividend+"  is divided by  "+divisor+" is " +remainder);
+        }
 
     }
     
