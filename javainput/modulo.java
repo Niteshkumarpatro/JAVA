a60b10cb15d45c1147032189d6dd0357a3af902a
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-11 13:56:22 +0530

    #10PROGRAM

--- javainput/modulo.java
+++ javainput/modulo.java
@@ -1,15 +1,17 @@
 package javainput;
 import java.util.Scanner;
 public class modulo{
-        public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.print(" Enter dividend ");
-        int dividend = sc.nextInt();
-        System.out.print(" Enter divisor ");
-        int divisor =sc.nextInt();
-        int remainder =dividend % divisor;
-        System.out.println("The remainder when "+dividend+"  is divided by  "+divisor+" is " +remainder);
+        public static void main(String[] args){ 
+            try (Scanner sc = new Scanner(System.in)) {
+                System.out.print(" Enter dividend ");
+                int dividend = sc.nextInt();
+                System.out.print(" Enter divisor ");
+                int divisor =sc.nextInt();
+                int remainder =dividend % divisor;
+                System.out.println("The remainder when "+dividend+"  is divided by  "+divisor+" is " +remainder);
+            }
+        }
 
     }
     
-}
+
