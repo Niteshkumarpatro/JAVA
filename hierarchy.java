b3899eb9f987dd0dac6de7286a38512074008c6f
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-12 22:01:22 +0530

    hierarchy

---
+++ hierarchy.java
@@ -0,0 +1,12 @@
+public class hierarchy {
+public static void main(String[] args) {
+      int i=2,j=3,k,l;
+      double a,b;
+      k=i/j*j;
+      j=j/i*j;  
+      a=i/j*j;  
+      b=j/i*i; 
+    System.out.println(k+" "+i+" "+a+" "+b);   
+    }
+    
+}
