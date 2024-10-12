d2e0e367868b1f1c1fadbebd9a3829c4f82aaa4b
Nitesh Kumar Patro <niteshkumarpatraa@gmail.com>
2024-10-12 22:01:41 +0530

    char datatype

---
+++ chardatatypye.java
@@ -0,0 +1,24 @@
+
+public class chardatatypye {
+public static void main(String[] args)
+    {  
+         char ch='a';
+        System.out.println(ch);
+        int x=(int)ch;
+        System.out.println(x);
+        }
+}
+// abcdefghijklmnopqrstuvwxyz
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+// !@#$%^&*()+{}|:"<>?/.,';][\=-]"
+// 0123456789
+ 
+// ASCII VALUES
+//  CHAR CH = 'a';        
+//  a-97                 A-65
+//  b-98                 B-66
+//  c-99                 C-67
+//  d-100                D-68
+//  ,,                    .. 
+//  y-121                 Y-89
+//  z-122                 Z-90
